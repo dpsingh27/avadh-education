56b02c13543658013860ef14585d5403d388a865
dpsingh27 <divyapratapsingh27@gmail.com>
2024-04-04 21:35:54 +0530

    Initial commit

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from "react"
 
 const Head = () => {
@@ -6,15 +7,27 @@ const Head = () => {
       <section className='head'>
         <div className='container flexSB'>
           <div className='logo'>
-            <h1>ACADEMIA</h1>
-            <span>ONLINE EDUCATION & LEARNING</span>
+            <h1>AWADH DEVELOPER</h1>
+            <span>WEBSITE DEVELOPMENT & LEARNING</span>
           </div>
 
           <div className='social'>
-            <i className='fab fa-facebook-f icon'></i>
-            <i className='fab fa-instagram icon'></i>
-            <i className='fab fa-twitter icon'></i>
-            <i className='fab fa-youtube icon'></i>
+            <a href="https://www.facebook.com/PratapDivyaSingh" target="_blank">
+              <i className='fab fa-facebook-f icon'></i>
+            </a>
+
+<a href="https://www.instagram.com/pratap.divya.singh/" target="_blank" rel="noopener noreferrer">
+    <i className="fab fa-instagram icon"></i>
+</a>
+
+<a href="https://twitter.com/PratapDivyaSin1" target="_blank" rel="noopener noreferrer">
+    <i className="fab fa-twitter icon"></i>
+</a>
+
+<a href="https://www.youtube.com/channel/UCHNDXuBbCdLA2sfbbkmMJPw" target="_blank" rel="noopener noreferrer">
+    <i className="fab fa-youtube icon"></i>
+</a>
+
           </div>
         </div>
       </section>
